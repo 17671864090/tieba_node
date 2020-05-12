dd64dd8a45811eab555e4f778efa558acbc57134
kandong <53746237@qq.com>
2020-05-12 10:57:07 +0800

    修改服务器端口

--- config/config.default.js
+++ config/config.default.js
@@ -24,6 +24,14 @@ module.exports = appInfo => {
   };
 
 
+  config.cluster = {
+    listen: {
+      path: '',
+      port: 9966,
+      hostname: '0.0.0.0',
+    }
+  };
+
 
   config.security = {
     csrf: {
