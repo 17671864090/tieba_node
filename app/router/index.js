99162450c1ef43824fd8e0b98c755fe8a9cfa287
kandong <53746237@qq.com>
2020-05-20 11:06:08 +0800

    更新支付系统

--- app/router/index.js
+++ app/router/index.js
@@ -5,7 +5,7 @@ module.exports = app =>{
 
     router.post('/api/admin/v1/login', controller.admin.user.login);//管理登录
     router.post('/api/admin/v1/register', controller.admin.user.register);//管理登
-    router.get('/api/admin/v1/img',gzip, controller.admin.user.img);//上传图片
+    router.get('/api/admin/v1/img', controller.admin.user.img);//上传图片
     router.post('/api/admin/v1/Release',gzip, controller.admin.user.release);
     router.post('/api/admin/v1/userinfo',gzip, controller.admin.user.userinfo);//获取用户基本信息
     router.get('/api/admin/v1/topics', controller.admin.user.topics);//获取文字列表
