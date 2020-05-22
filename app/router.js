881f39014064a7d9503465b5186a871c0f074030
kandong <53746237@qq.com>
2020-05-22 18:19:41 +0800

    修改支付回调地址

--- app/router.js
+++ app/router.js
@@ -13,6 +13,9 @@ module.exports = app => {
   // router.post('/topics', topic.put);
 
 
+  //支付系统
+  router.post('/alipay', controller.pay.index);
+
 
 
 
@@ -20,13 +23,8 @@ module.exports = app => {
 
   //物料精选{相似推荐,猜你喜欢}
   router.get('/user/Optimus/:material_id/:min_ids', controller.home.Optimus);
-
-
   router.get('/user/TpwdCreate', controller.home.TpwdCreate);
-
-
   router.get('/api/user/itemoptional/:q/:page_no', controller.home.itemoptional);
-
   require('./router/user')(app);
   require('./router/index')(app);
 };
